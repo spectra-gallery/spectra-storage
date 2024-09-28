67f117b9465a6bab65729403a1a6d9db4ce4a470
Philmomu <phil@philmo.ch>
2024-09-28 12:40:13 -0400

    portfolio

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,13 +1,16 @@
 const authJwt = require('./authJwt');
 const uploadFileMiddleware = require('./upload');
+const multipleUpload = require('./multipleUpload');
 const generateImg = require('./generateImg');
 const generatePreview = require('./generatePreview');
 const awsUpload = require("./awsUpload");
+// const { multipleUpload } = require('../controllers/fileUpload.controller');
 
 module.exports = {
   authJwt,
   uploadFileMiddleware,
   generateImg,
   generatePreview,
-  awsUpload
+  awsUpload,
+  multipleUpload
 };
